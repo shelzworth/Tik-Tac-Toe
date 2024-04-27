c7030923a5fc9232cd404b88df62abd50b374a00
sheldon <155117244+shelzworth@users.noreply.github.com>
2024-04-26 23:33:17 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace RealTicTac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox0 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.BtnRetry = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.LblPlayer2 = new System.Windows.Forms.Label();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox0
+            // 
+            this.pictureBox0.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox0.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox0.ErrorImage = null;
+            this.pictureBox0.Location = new System.Drawing.Point(211, 100);
+            this.pictureBox0.Name = "pictureBox0";
+            this.pictureBox0.Size = new System.Drawing.Size(173, 150);
+            this.pictureBox0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox0.TabIndex = 0;
+            this.pictureBox0.TabStop = false;
+            this.pictureBox0.Click += new System.EventHandler(this.pickedBox);
+            this.pictureBox0.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(406, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pickedBox);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(406, 266);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(173, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pickedBox);
+            this.pictureBox4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(609, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(173, 150);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pickedBox);
+            this.pictureBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(609, 266);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(173, 150);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pickedBox);
+            this.pictureBox5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(211, 266);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(173, 150);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pickedBox);
+            this.pictureBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox7.Location = new System.Drawing.Point(406, 426);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(173, 150);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 9;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pickedBox);
+            this.pictureBox7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox8.Location = new System.Drawing.Point(609, 426);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(173, 150);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pickedBox);
+            this.pictureBox8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Location = new System.Drawing.Point(211, 426);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(173, 150);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pickedBox);
+            this.pictureBox6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // BtnRetry
+            // 
+            this.BtnRetry.BackColor = System.Drawing.Color.Snow;
+            this.BtnRetry.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRetry.Location = new System.Drawing.Point(800, 514);
+            this.BtnRetry.Name = "BtnRetry";
+            this.BtnRetry.Size = new System.Drawing.Size(162, 62);
+            this.BtnRetry.TabIndex = 10;
+            this.BtnRetry.Text = "Retry";
+            this.BtnRetry.UseVisualStyleBackColor = false;
+            this.BtnRetry.Click += new System.EventHandler(this.BtnRetry_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Snow;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(12, 514);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(162, 62);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // LblPlayer2
+            // 
+            this.LblPlayer2.AutoSize = true;
+            this.LblPlayer2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPlayer2.Location = new System.Drawing.Point(837, 266);
+            this.LblPlayer2.Name = "LblPlayer2";
+            this.LblPlayer2.Size = new System.Drawing.Size(96, 28);
+            this.LblPlayer2.TabIndex = 12;
+            this.LblPlayer2.Text = "Player 2";
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.Location = new System.Drawing.Point(46, 266);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(96, 28);
+            this.lblPlayer1.TabIndex = 13;
+            this.lblPlayer1.Text = "Player 1";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox9.Image = global::RealTicTac.Properties.Resources.slurp;
+            this.pictureBox9.Location = new System.Drawing.Point(817, 124);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(133, 126);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 14;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox10.Image = global::RealTicTac.Properties.Resources.c2;
+            this.pictureBox10.Location = new System.Drawing.Point(25, 118);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(149, 132);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 15;
+            this.pictureBox10.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RealTicTac.Properties.Resources.ttt;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(974, 588);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.LblPlayer2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.BtnRetry);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox0);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox0;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button BtnRetry;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label LblPlayer2;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace RealTicTac
+{
+    public partial class Form1 : Form
+    { 
+        //empty array, variables and sound player
+        string[] board = { "", "", "", "", "", "", "", "", "", };
+        int turn = 0;
+        bool slurpwin = false;
+        bool crownwin = false;
+        int pbPicked;
+        SoundPlayer boogie = new SoundPlayer(Properties.Resources.Boogie_Down__Fortnite____QuickSounds_com);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        
+        private void pickedBox(object sender, EventArgs e)
+        {
+            //Identifying box clicked
+            turn++;
+            PictureBox picked = (PictureBox)sender;
+            picked.Enabled = false;
+            char[] pbpickedname = picked.Name.ToCharArray();
+            pbPicked = int.Parse(pbpickedname[10].ToString());
+
+            //turn distinguishment
+            switch (turn % 2)
+            {
+                case 0:
+                    picked.Image = Properties.Resources.slurp;
+                    board[pbPicked] = "slurp";
+                    whosethewinner();
+                    break;
+                case 1:
+                    picked.Image = Properties.Resources.c2;
+                    board[pbPicked] = "crown";
+                    whosethewinner();
+                    break;
+
+            }
+        }
+
+        private void whosethewinner()
+        {
+            //checkin who makes 3 in a row
+            //crown winner 
+            if (board[0] == "crown" && board[1] == "crown" && board[2] == "crown")
+            {
+                MessageBox.Show("The Crowners take Tilted Towers!");
+                BtnRetry.Visible = true;
+            }
+            if (board[3] == "crown" && board[4] == "crown" && board[5] == "crown")
+            {
+                MessageBox.Show("The Crowners take Tilted Towers!");
+                BtnRetry.Visible = true;
+            }
+            if (board[6] == "crown" && board[7] == "crown" && board[8] == "crown")
+            {
+                MessageBox.Show("The Crowners take Tilted Towers!");
+                BtnRetry.Visible = true;
+            }
+            if (board[0] == "crown" && board[3] == "crown" && board[6] == "crown")
+            {
+                MessageBox.Show("The Crowners take Tilted Towers!");
+                BtnRetry.Visible = true;
+            }
+            if (board[1] == "crown" && board[4] == "crown" && board[7] == "crown")
+            {
+                MessageBox.Show("The Crowners take Tilted Towers!");
+                BtnRetry.Visible = true;
+            }
+            if (board[2] == "crown" && board[5] == "crown" && board[8] == "crown")
+            {
+                MessageBox.Show("The Crowners take Tilted Towers!");
+                BtnRetry.Visible = true;
+            }
+            if (board[0] == "crown" && board[4] == "crown" && board[8] == "crown")
+            {
+                MessageBox.Show("The Crowners take Tilted Towers!");
+                BtnRetry.Visible = true;
+            }
+            if (board[2] == "crown" && board[4] == "crown" && board[6] == "crown")
+            {
+                MessageBox.Show("The Crowners take Tilted Towers!");
+                BtnRetry.Visible = true;
+            }
+            //slurp winner
+            if (board[0] == "slurp" && board[1] == "slurp" && board[2] == "slurp")
+            {
+                MessageBox.Show("The Slurpers take Tilted Towers!");
+                BtnRetry.Visible = true;
+
+            }
+            if (board[3] == "slurp" && board[4] == "slurp" && board[5] == "slurp")
+            {
+                MessageBox.Show("The Slurpers take Tilted Towers!");
+                BtnRetry.Visible = true;
+
+            }
+            if (board[6] == "slurp" && board[7] == "slurp" && board[8] == "slurp")
+            {
+                MessageBox.Show("The Slurpers take Tilted Towers!");
+                BtnRetry.Visible = true;
+
+            }
+            if (board[0] == "slurp" && board[3] == "slurp" && board[6] == "slurp")
+            {
+                MessageBox.Show("The Slurpers take Tilted Towers!");
+                BtnRetry.Visible = true;
+
+            }
+            if (board[1] == "slurp" && board[4] == "slurp" && board[7] == "slurp")
+            {
+                MessageBox.Show("The Slurpers take Tilted Towers!");
+                BtnRetry.Visible = true;
+
+            }
+            if (board[2] == "slurp" && board[5] == "slurp" && board[8] == "slurp")
+            {
+                MessageBox.Show("The Slurpers take Tilted Towers!");
+                BtnRetry.Visible = true;
+
+            }
+            if (board[0] == "slurp" && board[4] == "slurp" && board[8] == "slurp")
+            {
+                MessageBox.Show("The Slurpers take Tilted Towers!");
+                BtnRetry.Visible = true;
+
+            }
+            if (board[2] == "slurp" && board[4] == "slurp" && board[6] == "slurp")
+            {
+                MessageBox.Show("The Slurpers take Tilted Towers!");
+                BtnRetry.Visible = true;
+
+            }
+            if (turn > 9)
+            {
+                MessageBox.Show("Nobody wins! The battle rages on!");
+                BtnRetry.Visible = true;
+
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //hid play again button and background music
+            BtnRetry.Visible = false;
+            boogie.Play();
+            
+        }
+
+        private void BtnRetry_Click(object sender, EventArgs e)
+        {
+            //replay ability
+            Application.Restart();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //exit button
+            Application.Exit();
+        }
+    }
+}
